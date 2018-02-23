2f5ede1a4c9fcff0fea5efd023af5d380d3f1409
jrand35 <jrand35@aol.com>
2018-02-23 18:37:51 -0500

    Added sounds, created scene

---
+++ Scenes/Coin.gd
@@ -0,0 +1,18 @@
+extends AudioStreamPlayer3D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _input(event):
+    if event.is_action_pressed("ui_cancel"): play();
